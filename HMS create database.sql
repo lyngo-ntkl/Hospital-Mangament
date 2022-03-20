3d666df79c982549f0e2149a2ccd48255732a780
lyngo-ntkl <93430314+lyngo-ntkl@users.noreply.github.com>
2022-03-20 17:09:03 +0700

    Update HMS create database.sql

--- HMS create database.sql
+++ HMS create database.sql
@@ -5,4 +5,72 @@ GO
 USE HospitalManagement
 GO
 
-CREATE TABLE 
+DROP TABLE IF EXISTS Hospital;
+CREATE TABLE Hospital(
+	hospitalID INT,
+	hospitalName NVARCHAR(50),
+	hospitalAddress NVARCHAR(200),
+	CONSTRAINT PK_HOSPITAL PRIMARY KEY(hospitalID)
+)
+
+DROP TABLE IF EXISTS HospitalContact;
+CREATE TABLE HospitalContact(
+	contactID INT,
+	hospitalID INT,
+
+)
+
+DROP TABLE IF EXISTS Doctor;
+CREATE TABLE Doctor(
+	doctorID VARCHAR(20),
+	firstName NVARCHAR(50),
+	middleName NVARCHAR(50),
+	lastName NVARCHAR(50),
+	birthDate Date,
+	address NVARCHAR(210),
+	gender CHAR,
+	
+	CONSTRAINT PK_DOCTOR PRIMARY KEY(doctorID),
+	CONSTRAINT CHK_DOCTOR_GENDER CHECK(GENDER='M' OR GENDER='F')
+)
+
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department(
+	DepartmentID int,
+	DepartmentName nvarchar(120),
+	ManagerID varchar(20),
+	StartDate Date,
+	CONSTRAINT PK_DEPARTMENT PRIMARY KEY(DepartmentID),
+	CONSTRAINT UQ_DEPARTMENT UNIQUE(ManagerID)
+)
+
+DROP TABLE IF EXISTS Patient;
+CREATE TABLE Patient(
+	patientID INT,
+	firstName NVARCHAR(50),
+	middleName NVARCHAR(50),
+	lastName NVARCHAR(50),
+	nationality VARCHAR(50),
+	gender CHAR,
+	address NVARCHAR(200),
+	birthDate DATE,
+	phoneNumber VARCHAR(20),
+	CONSTRAINT PK_PATIENT PRIMARY KEY(patientID),
+	CONSTRAINT CHK_PATIENT_GENDER CHECK(gender='M' OR gender='F'),
+	CONSTRAINT CHK_PATIENT_PHONE CHECK(phoneNumber NOT LIKE '%[^0-9]%')
+)
+GO
+
+DROP TABLE IF EXISTS MedicalHistory;
+CREATE TABLE MedicalHistory
+(
+	medicalHistoryID INT,
+	patientID INT,
+	disease VARCHAR(50),
+	medication VARCHAR(50),
+	allergy VARCHAR(50),
+	surgery VARCHAR(50),
+	socialHistory VARCHAR(50),
+	familyHistory VARCHAR(50)
+	CONSTRAINT PK
+)
